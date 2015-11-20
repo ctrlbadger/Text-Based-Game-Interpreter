407b3e1deb3e9ece5a0a8ef763e6c4f9c0610fd0
ctrlbadger <broadbentt19@wells-cathedral-school.com>
2015-11-20 20:39:51 +0000

    Version 2.0

--- TB-Law.cpp
+++ TB-Law.cpp
@@ -6,91 +6,106 @@
 struct Text {
 	std::string TextRead = "";
 	int NameText = 0;
-	std::vector<int>  DirAllocator = { 0, 0, 0, 0, 0, 0, 0 };
-	std::vector<std::string> DirText = { "", "", "", "", "", "", "" };
+	int  DirAllocator[7] = { 0,0,0,0,0,0,0 };
+	std::string DirText[7] = { "","","","","","","" };
 
 };
 
 
+bool isNum(char String) {
+	if (String == '0' || String == '1' || String == '2' || String == '3' || String == '4' || String == '5' || String == '6' || String == '7' || String == '8' || String == '9') {
+		return true;
+	}
+	else {
+		return false;
+	}
+}
+/*
+Text ClearText(Text ZeroText) {
+ZeroText.NameText = 0;
+ZeroText.TextRead = "";
+for (unsigned int Count = 0; Count < 7; Count++) {
+ZeroText.DirAllocator[Count] = 0;
+ZeroText.DirText[Count] = "";
+}
+return ZeroText;
+}
+*/
+Text FindDialog(int Directory) {
+	std::ifstream FileStream;
+	Text FindText;
+	std::string FileString = "";
+	FileStream.open("LawText.txt");
+	bool FoundText = false;
+	unsigned int Count = 0;
+
+
+	if (!FileStream.is_open()) {
+		std::cerr << "ERROR ERROR, DATA CORRUPTION\n DOGE DOGE DOGE DOGGGE DOGGE\n010101011001100010100101010101010010001001" << std::endl
+			<< "We cant find LawText.txt" << std::endl << ">>> Press Enter To Continue<<<";
+		std::cin.ignore();
+	}
+
+
+	do {
+		std::getline(FileStream, FileString);
+		if (isNum(FileString[0]) == true && isNum(FileString[1]) == true && FileString[2] != ':') {
+			FindText.NameText = (10 * (FileString[0] - '0')) + (FileString[1] - '0');
+			if (FindText.NameText == Directory) {
+				FoundText = true;
+			}
+
+		}
+		else if (isNum(FileString[0]) == true && FileString[1] != ':') {
+			FindText.NameText = (FileString[0] - '0');
+			if (FindText.NameText == Directory) {
+				FoundText = true;
+			}
+		}
+	} while (FoundText != true);
+
+
+	std::getline(FileStream, FileString);
+	do {
+		FindText.TextRead = FindText.TextRead + "\n" + FileString;
+		std::getline(FileStream, FileString);
+	} while (FileString != "#");
+	Count = 0;
+	std::getline(FileStream, FileString);
+	do {
+		if (FileString[1] == ':') {
+			FindText.DirAllocator[Count] = (FileString[0] - '0');
+			FileString.erase(0, 2);
+			FindText.DirText[Count] = FileString;
+		}
+		else {
+			FindText.DirAllocator[Count] = (10 * (FileString[0] - '0')) + (FileString[1] - '0');
+			FileString.erase(0, 3);
+			FindText.DirText[Count] = FileString;
+		}
+		Count++;
+		std::getline(FileStream, FileString);
+	} while (FileString != "~");
+	FileStream.close();
+	return FindText;
+}
 
 
 int main() {
-	// Note: You should include LawText.txt in the same directory as the executable
-	std::string FileName = "LawText.txt";
-	// This is where we hold all text that the game plays
-	std::vector<Text> VecText;
-	// Count is used for making sure I dont get errors
+	// Note: You should include LawText.txt in the same directory as the executable plus the required dll file
+	Text MainText;
+	MainText = FindDialog(0);
 	unsigned int Count = 0;
-	// This is used for all operations with the file
-	std::fstream File;
-	// Used to store the Strings as we read them into the file
-	std::string StrLine = "";
-	// "Option" is for entering the directorys of text
-	std::string Mode = "Text";
-	//
-	bool OptionMode = true;
-	File.open(FileName);
-	if (!File.is_open()) {
-
-		std::cerr << "ERROR ERROR, DATA CORRUPTION\n DOGE DOGE DOGE DOGGGE DOGGE\n010101011001100010100101010101010010001001" << std::endl;
-		std::cin.ignore();
-		return -1;
-	}
-	char DirectoryNum;
-	while (std::getline(File, StrLine)) {
-		if (StrLine == "0" || StrLine == "1" || StrLine == "2" || StrLine == "3" || StrLine == "4" || StrLine == "5" || StrLine == "6" || StrLine == "7" || StrLine == "8" || StrLine == "9") {
-			VecText.push_back(Text());
-			VecText[Count].NameText = atoi(StrLine.c_str());
-			Mode = "Text";
-		}
-		else if (Mode == "Text" && StrLine != "#") {
-			VecText[Count].TextRead += StrLine;
-		}
-		else if (Mode == "Option") {
-			OptionMode = true;
-			int OptionCount = 0;
-			do {
-
-				if (StrLine == "~") {
-					Count++;
-					OptionMode = false;
-				}
-				else {
-					if (StrLine[1] == ':') {
-						DirectoryNum = StrLine[0];
-						StrLine.erase(0, 1);
-					}
-					else {
-						DirectoryNum = (StrLine[0] * 10) + StrLine[1];
-						StrLine.erase(0, 2);
-					}
-					VecText[Count].DirAllocator[OptionCount] = (DirectoryNum - '0');
-					VecText[Count].DirText[OptionCount] = StrLine;
-					std::getline(File, StrLine);
-					OptionCount++;
-				}
-			} while (OptionMode);
-			Mode = "Text";
-		}
-		else if (Mode == "Text" && StrLine == "#") {
-			Mode = "Option";
-		}
-	}
-	// End Of Game
-	Count = 0;
-	unsigned int DirCount = 0;
-	int Answer;
-	std::vector<int>  DirAllocatorCount(5);
+	unsigned int Answer = 0;
 	do {
-		// This shows the text in the file
-		std::cout << VecText[Count].TextRead << std::endl;
-		for (DirCount = 0; VecText[Count].DirAllocator.size() > DirCount && VecText[Count].DirAllocator[DirCount] != 0; DirCount++) {
-			std::cout << (DirCount + 1) << ": " << VecText[Count].DirText[DirCount] << std::endl;
-			DirAllocatorCount[DirCount] = VecText[Count].DirAllocator[DirCount];
+
+		std::cout << MainText.TextRead << std::endl << std::endl;
+		for (Count = 0; MainText.DirText[Count] != ""; Count++) {
+			std::cout << (Count + 1) << ": " << MainText.DirText[Count] << std::endl;
 		}
-		std::cout << ": ";
+		std::cout << "\n: ";
 		std::cin >> Answer;
-		Count = Answer -= 1;
+		MainText = FindDialog(MainText.DirAllocator[Answer - 1]);
 	} while (true);
 	return 1;
 }
